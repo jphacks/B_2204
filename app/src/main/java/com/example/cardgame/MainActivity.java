7b52668ab28df60f038e73512a3beedbd6a0bfb7
poteto <67512297+poteto0@users.noreply.github.com>
2022-09-19 09:49:32 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/cardgame/MainActivity.java
+++ app/src/main/java/com/example/cardgame/MainActivity.java
@@ -44,6 +44,48 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    /*
+    public List getAll(){
+        // DB読み込み
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        // queryのselect
+        String[] projection = {
+                BaseColumns._ID,
+                FeedReaderContract.StudyEntry.COLUMN_NAME_SUBJECT,
+                FeedReaderContract.StudyEntry.COLUMN_NAME_DATE,
+                FeedReaderContract.StudyEntry.COLUMN_NAME_TIME
+        };
+
+        // [Where] 条件式
+        String selection = FeedReaderContract.StudyEntry.COLUMN_NAME_SUBJECT + " = ?";
+        String[] selectionArgs = { "" };
+
+        // How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                FeedReaderContract.StudyEntry.COLUMN_NAME_DATE + " DESC";
+
+        Cursor cursor = db.query(
+                FeedReaderContract.StudyEntry.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                null,              // The columns for the WHERE clause
+                null,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
+
+        List itemIds = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            long itemId = cursor.getLong(
+                    cursor.getColumnIndexOrThrow(FeedReaderContract.StudyEntry._ID));
+            itemIds.add(itemId);
+        }
+        cursor.close();
+
+        return itemIds;
+    }
+    */
 
     /*
     @Override
